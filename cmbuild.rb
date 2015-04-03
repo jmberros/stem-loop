c58ac74d8d2c0c311299bd7d1811dc3873adb86e
Juan Manuel Berros <juanmaberros@gmail.com>
2015-04-03 00:36:38 -0300

    cmbuild ruby wrapper with cmcalibrate forecast

---
+++ cmbuild.rb
@@ -0,0 +1,30 @@
+#! /usr/bin/env ruby
+# encoding: utf-8
+
+require 'colorize'
+
+def cmbuild(stockholm_filename)
+  name = stockholm_filename.gsub ".sto", ""
+
+  puts "\n Build Covariance Model for '#{stockholm_filename}'"
+  `cmbuild -F #{name}.cm #{name}.sto` # | tee #{name}.cm.log`
+
+  puts " ↪ #{name}.cm".green.bold + \
+       #"\n ( There's a log of the process in #{name}.cm.log )"
+
+  forecast_calibration "#{name}.cm"
+end
+
+def forecast_calibration(cm_filename)
+  puts "\n Forecast Calibration of '#{cm_filename}'"
+  command = "cmcalibrate --nforecast 6 --forecast #{cm_filename} | "\
+            "grep -v '^#' | grep -v 'ok' | awk '{ print $2 }'"
+  predicted_time = `#{command}`.chomp
+  puts " ↪ calibration should take ~#{predicted_time} (hh:mm:ss)".blue.bold
+end
+
+if __FILE__ == $0
+  abort(" 😸 Feed me one -just one- input stockholm filename") unless ARGV.count == 1
+
+  cmbuild ARGV.first
+end
