63b3d23fcb58b85edd8ae02ec8d365e4167a16d6
Juan Manuel Berros <juanmaberros@gmail.com>
2015-04-12 03:02:10 -0300

    hacks for the nasty python script to download scaffold

--- bin/download-scaffold.py
+++ bin/download-scaffold.py
@@ -2,24 +2,26 @@
 #-*- encoding: utf-8 -*-
 
 import sys
-sys.path.insert(0, "../lib") # TODO: Do this in a less hacky way
-import mechabrowser
+import mechanize
+import cookielib
 from bs4 import BeautifulSoup
 
 class ScaffoldDownloader:
+    def __init__(self):
+        self.browser = self.create_browser()
+        self.login()
+
     def download(self, scaffold_id):
-        browser = mechabrowser.create_browser()
-        self.login(browser)
-        browser.open(self.scaffold_detail_url(scaffold_id))
+        self.browser.open(self.scaffold_detail_url(scaffold_id))
         
-        for link in browser.links(url_regex="metaDetail"):
+        for link in self.browser.links(url_regex="metaDetail"):
             description = link.text
 
-        for link in browser.links(url_regex="MetaScaffoldGraph"):
+        for link in self.browser.links(url_regex="MetaScaffoldGraph"):
             sequence_url = "https://img.jgi.doe.gov/cgi-bin/m/" + \
                            link.url.replace("page=metaScaffoldGraph",
                                             "page=metaScaffoldDna")
-            dna_page = BeautifulSoup(browser.open(sequence_url).read())
+            dna_page = BeautifulSoup(self.browser.open(sequence_url).read())
             sequence = dna_page.find("textarea", {'name': "fasta"}).text
 
         filename = self.save_to_file(scaffold_id, description, sequence)
@@ -32,13 +34,13 @@ class ScaffoldDownloader:
             file.write(sequence)
         return filename
 
-    def login(self, browser):
-        browser.open("https://img.jgi.doe.gov/m/")
-        browser.select_form("imghome")
-        browser["login"] = "jmberros"
-        browser["password"] = "jmberros"[::-1]
-        browser.submit()
-        return browser
+    def login(self):
+        self.browser.open("https://img.jgi.doe.gov/m/")
+        self.browser.select_form("imghome")
+        self.browser["login"] = "jmberros"
+        self.browser["password"] = "jmberros"[::-1]
+        self.browser.submit()
+        return self.browser
 
     def scaffold_detail_url(self, scaffold_id):
         return "https://img.jgi.doe.gov/cgi-bin/m/main.cgi?" + \
@@ -66,6 +68,34 @@ class ScaffoldDownloader:
 
     def metagenome_id(self, scaffold_id):
         return "_".join(scaffold_id.split("_")[:-1])
+
+    def create_browser(self):
+        # Browser
+        br = mechanize.Browser()
+
+        # Cookie Jar
+        cj = cookielib.LWPCookieJar()
+        br.set_cookiejar(cj)
+
+        # Browser options
+        br.set_handle_equiv(True)
+        br.set_handle_gzip(True)
+        br.set_handle_redirect(True)
+        br.set_handle_referer(True)
+        br.set_handle_robots(False)
+
+        # Follows refresh 0 but not hangs on refresh > 0
+        br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+        # Want debugging messages?
+        #br.set_debug_http(True)
+        #br.set_debug_redirects(True)
+        #br.set_debug_responses(True)
+
+        # User-Agent (this is cheating, ok?)
+        br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+        return br
 
 
 if __name__ == "__main__":
