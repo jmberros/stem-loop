ff2961459d1bc23e40d7c9fca61ad1e6f2e97176
Juan Manuel Berros <juanmaberros@gmail.com>
2015-05-06 18:48:15 -0300

    added --cpu option to cmsearch.rb

--- bin/cmsearch.rb
+++ bin/cmsearch.rb
@@ -4,7 +4,7 @@
 require_relative '../stem-loop'
 
 def main(opts={})
-  opts[:max] = ARGV.any? { |arg| arg == '--max' }
+  opts[:max] = ARGV.any? { |arg| arg == '--max' } # TODO: This should be optparsed earlier
   fastas = Dir["*.fa"] + Dir["*.fna"]
   models = Dir["*.c.cm"]
 
@@ -20,11 +20,28 @@ def main(opts={})
 
       $logger.debug "➡ cmsearch #{covariance_model} in #{fasta}"
 
-      `cmsearch #{'--max' if opts[:max]} --tblout #{filename}.cmsearch.tbl #{covariance_model} #{fasta} > #{filename}.cmsearch-output`
+      `cmsearch --tblout #{filename}.cmsearch.tbl \
+       #{'--max' if opts[:max]} #{'--cpu ' + opts[:cpu] if opts[:cpu]} \
+       #{covariance_model} #{fasta} > #{filename}.cmsearch-output`
     end
   end
 end
 
+if __FILE__ == $0
+  options = {}
+  OptionParser.new do |opts|
+    opts.banner = "Usage: #{__FILE__}.rb [options]"
 
-main if __FILE__ == $0
+    opts.on("--cpu=N", "Use n cores") do |cpu|
+      options[:cpu] = cpu
+    end
+
+    opts.on("-h", "--help", "Prints this help") do
+      puts opts
+      exit
+    end
+  end.parse!
+
+  main(options)
+end
 
