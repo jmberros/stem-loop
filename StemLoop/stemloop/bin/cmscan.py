857c6b76e44f025641c8ac0f8dfd76683e532b52
Juan Manuel Berros <juanmaberros@gmail.com>
2015-06-19 10:23:06 -0300

    fixed cmscan.py script

--- StemLoop/stemloop/bin/cmscan.py
+++ StemLoop/stemloop/bin/cmscan.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import sys, os, argparse
-import fileinput
 
 bin_dir = os.path.dirname(os.path.realpath(__file__))
 top_dir = os.path.dirname(bin_dir)
@@ -12,20 +11,14 @@ from lib.infernal import Infernal
 
 
 if __name__ == "__main__":
-    if sys.stdin.isatty():
-        description = "Scan all fastas in this dir with a CM database"
-        parser = argparse.ArgumentParser(description=description)
-        parser.add_argument("cm_database")
-        parser.add_argument("--fasta", help="Limit search to target fasta")
-        parser.add_argument("--cpu", help="CPU cores to use (default=1)",
-                            default=1, type=int)
-        options = vars(parser.parse_args())
+    description = "Scan all fastas in this dir with a CM database"
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("cm_database")
+    parser.add_argument("--fasta", help="Limit search to target fasta")
+    parser.add_argument("--cpu", help="CPU cores to use (default=1)",
+                        default=1, type=int)
+    options = vars(parser.parse_args())
 
-        Infernal().cmscan(
-            options["cm_database"], options["fasta"], options["cpu"]
-        )
-    else:
-        for line in fileinput.input():
-            filename = line.strip()
-            if len(filename) > 0:
-                Infernal().cmscan(filename)
+    Infernal().cmscan(
+        options["cm_database"], options["fasta"], options["cpu"]
+    )
